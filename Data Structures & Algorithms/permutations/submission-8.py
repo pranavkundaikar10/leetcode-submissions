73d748315dacc4a134be9122240ca3c47aec83e3
pranavkundaikar10 <kundaikarpranav10+git@gmail.com>
2026-08-04 11:49:37 -0400

    Add: permutations - submission-8

---
+++ Data Structures & Algorithms/permutations/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(subset, visited):
+            if len(subset) == len(nums):
+                res.append(subset[:])
+
+            for j in range(0, len(nums)):
+                if visited[j]:
+                    continue
+                visited[j] = True
+                subset.append(nums[j])
+                dfs(subset, visited)
+                visited[j] = False
+                subset.pop()
+
+
+        dfs([], [False] * len(nums))
+        return res
