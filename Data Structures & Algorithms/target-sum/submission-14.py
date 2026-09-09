ea3c661d25804d6dc06d0675d70e324162c7771e
pranavkundaikar10 <kundaikarpranav10+git@gmail.com>
2026-09-09 16:07:50 -0400

    Add: target-sum - submission-14

---
+++ Data Structures & Algorithms/target-sum/submission-14.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+
+        def backtrack(i, total):
+            if i >= len(nums):
+                return 1 if total == target else 0
+                
+            key = (i, total)
+            if key in memo:
+                return memo[key]
+            
+            memo[key] = backtrack(i+1, total+nums[i]) + backtrack(i+1, total-nums[i])
+            return memo[key]
+
+        return backtrack(0, 0)
