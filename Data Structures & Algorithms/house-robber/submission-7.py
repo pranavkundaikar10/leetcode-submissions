a8ad88c65c2515e41828f82feac4f39991bbb8b6
pranavkundaikar10 <kundaikarpranav10+git@gmail.com>
2026-09-11 14:35:15 -0400

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        def backtrack(i, total):
+            if i >= len(nums):
+                return total
+            key = (i, total)
+            if key in memo:
+                return memo[key]
+
+            memo[key] = max(backtrack(i+1, total), backtrack(i+2, total+nums[i]))
+            return memo[key]
+
+        return backtrack(0, 0)
